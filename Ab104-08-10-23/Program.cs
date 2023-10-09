685be73192cf699387cf732566db8d92e7513807
tofiq7770 <tofigtn@code.edu.az>
2023-10-09 06:05:21 +0400

    Adding files

---
+++ Ab104-08-10-23/Program.cs
@@ -0,0 +1,110 @@
+﻿
+using Ab104;
+
+//Student[] students = new Student[] 
+//{
+//new Student("Ali", "Shukurlu", "Adnsu", 49),
+
+//};
+
+//foreach (var student in students)
+//{
+//  student.GetInfo();
+//  student.CheckGraduationStatus();
+//  student.GetDiplomDegree();
+
+//   Console.WriteLine("-------------------------");
+
+//}
+
+
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int reshadAge=0;
+//        int aydanAge=0;
+//        for (int i = 0; i < 4; i++)
+//        {
+//            Console.WriteLine("Enter Reshad Point");
+//            int reshadPoint = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Enter Aydan Point");
+//            int aydanPoint = int.Parse(Console.ReadLine());
+//            int result = GetByCompetition(reshadPoint, aydanPoint, ref reshadPoint, ref aydanPoint);
+//            Console.WriteLine(result);
+//        }
+//    }
+//    public static int GetByCompetition(int n1,int n2,ref int n3,ref int n4)
+//    {
+//        if (n1 > n2)
+//        {
+//            n3++;
+//            return n3;
+//        }
+//        else if (n1 < n2)
+//        {
+//            n4++;
+//            return n4;
+//        }
+//        else
+//        {
+//            return 0;
+//        }
+//    }
+//}
+
+
+//internal class Program
+//{
+//    static void Main()
+//    {
+//        string phrase = Console.ReadLine();
+
+//        string[] words = SplitByWord(phrase);
+
+//        Console.WriteLine("Sozler:");
+//        foreach (string item in words)
+//        {
+//            Console.WriteLine(item);
+//        }
+//    }
+
+//    static string[] SplitByWord(string phrase)
+//    {
+//        string[] words = phrase.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+//        return words;
+//    }
+//}
+
+
+//internal class Program
+//{
+//    static void Main()
+//    {
+//        string withoutTrim = Console.ReadLine();
+//        string trim = TrimByWords(withoutTrim);
+
+//        Console.WriteLine("Without Trim: " + withoutTrim);
+//        Console.WriteLine("With Trim: " + trim);
+//    }
+
+//    static string TrimByWords(string phrase)
+//    {
+//        int start = 0;
+//        while (start < phrase.Length && char.IsWhiteSpace(phrase[start]))
+//        {
+//            start++;
+//        }
+//        phrase = phrase.Substring(start);
+
+//        int end = phrase.Length - 1;
+//        while (end >= 0 && char.IsWhiteSpace(phrase[end]))
+//        {
+//            end--;
+//        }
+//        phrase = phrase.Substring(0, end + 1);
+
+//        return phrase;
+//    }
+//}
---
+++ Ab104-08-10-23/Student.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ab104
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool IsGraduated;
+
+        public Student(string name, string surname, string group, int point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = CheckGraduation();
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($" {Name}");
+            Console.WriteLine($" {Surname}");
+            Console.WriteLine($" {Group}");
+            Console.WriteLine($" {Point}");
+            Console.WriteLine($"{IsGraduated}");
+        }
+        public string CheckGraduationStatus()
+        {
+            if (IsGraduated)
+            {
+                return ($"{Name} {Surname}  mezun olub.");
+
+            }
+
+            else
+            {
+                return($"{Name} {Surname} mezun olmayıb.");
+            }
+
+        }
+        private bool CheckGraduation()
+        {
+            return Point > 65;
+        }
+        public string GraduationStatus()
+        {
+            if (IsGraduated)
+            {
+                return ($"{Name} {Surname}  mezun olub.");
+
+            }
+
+            else
+            {
+                return ($"{Name} {Surname} mezun olmayıb.");
+            }
+
+        }
+        private bool Graduation()
+        {
+            return Point > 65;
+        }
+        public void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("Diplom NO.");
+            }
+            else if (Point >= 65 && Point < 80)
+            {
+                Console.WriteLine("Simple diplom.");
+            }
+            else if (Point >= 80 && Point < 90)
+            {
+                Console.WriteLine("Honor.");
+            }
+            else
+            {
+                Console.WriteLine("High Honor diplom.");
+            }
+
+        }
+    }
+}
